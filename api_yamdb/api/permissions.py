412999dd870cd3c0c636dc6e82d9a3fae1a55f86
Александр Меньших <AlMen89@Yandex.ru>
2022-08-17 21:50:44 +0400

    permissions refactoring

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,18 +1,20 @@
 from rest_framework import permissions
 
 
+def admin_access(request):
+    """Проверяет наличие прав админа/суперпользователя."""
+    return (hasattr(request.user, 'role') and request.user.role == 'admin'
+            or request.user.is_superuser)
+
+
 class AdminOrSuperuserOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.role == 'admin' or request.user.is_superuser
-        )
+        return request.user.is_authenticated and admin_access(request)
 
 
 class AdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or (hasattr(request.user, 'role')
-                    and request.user.role == 'admin')
-                )
+                or admin_access(request))
